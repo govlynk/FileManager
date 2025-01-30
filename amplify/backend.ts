e3b8274020cc570280b5e36691d7a061483001d5
GovLynk <admin@govlynk.com>
2025-01-30 16:11:09 -0600

    updated amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,13 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { storage } from "./storage/resource";
+import { samApi } from "./functions/samApi/resource";
 
-defineBackend({
-  auth,
-  data,
+export const backend = defineBackend({
+	auth,
+	storage,
+	data,
+	// Define the function as a custom resource
+	samApi,
 });
