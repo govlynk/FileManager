7275d04e376190bf68a82066384a75b653ced4bb
GovLynk <admin@govlynk.com>
2025-01-30 16:18:07 -0600

    fixed backend issue

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,12 +2,9 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { storage } from "./storage/resource";
-import { samApi } from "./functions/samApi/resource";
 
 export const backend = defineBackend({
 	auth,
 	storage,
 	data,
-	// Define the function as a custom resource
-	samApi,
 });
